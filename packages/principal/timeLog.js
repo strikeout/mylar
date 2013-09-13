d54aebf140e8d4198450516b411c301ba15f24aa
raluca <raluca@ubuntu.(none)>
2013-09-13 12:32:55 -0400

    timing for eval

--- packages/principal/timeLog.js
+++ packages/principal/timeLog.js
@@ -3,3 +3,11 @@ timeLog = "";
 logAdd = function(msg) {
     timeLog = timeLog + msg;
 }
+
+startTime = function(msg) {
+    logAdd(msg + " ST "  + window.performance.now() + "\n");
+}
+
+endTime = function(msg) {
+    logAdd(msg + " END " +  window.performance.now() + "\n");
+}
