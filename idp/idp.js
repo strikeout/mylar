011b5c506686089f565bb2e2ffc6364195f3fe2b
Raluca Ada Popa <raluca@csail.mit.edu>
2013-09-18 19:32:35 -0400

    idp keys

--- idp/idp.js
+++ idp/idp.js
@@ -3,7 +3,27 @@ if (Meteor.isClient) {
     Deps.autorun(function () {
         Meteor.subscribe("userdata");
     });
+/*
 
+    //EVENTS HANDLERS BUTTON
+    Template.hello.events({
+	'click #loginBtn': function(evt) {
+	    CreateUser();
+	},
+	'click #logoutBtn': function(evt) {
+	    //UPDATE USER COLLECTION ONLINE FALSE
+	    Meteor.call('UpdateOnlineFalse');
+	    Meteor.logout();
+	},
+	'keypress #formCreateUser': function(evt) {
+	    if (evt.keyCode == 13){
+		CreateUser();
+		return false;
+	    }
+	}
+    });
+    
+*/
     Template.hello.user_email = function () {
         var user = Meteor.users.findOne();
         if (user) {
@@ -19,22 +39,24 @@ if (Meteor.isClient) {
         }
     };
 
-    
+}
+ 
 
-    // calls cb with an application specific key
-    get_app_key = function(origin) {
-	Meteor.call("get_app_key", origin, on_complete);
-    }
-
-    // calls cb with a certificate
-    certify_pk = function(pk, origin, cb) {
-	Meteor.call("certify_pk", pk, on_complete);
-    }
-
+// calls cb with an application specific key
+get_app_key = function(origin, cb) {
+    Meteor.call("get_app_key", origin, cb);
 }
 
-idpkeys = base_crypto.generate_keys();
+// calls cb with a certificate
+certify_pk = function(pk, origin, cb) {
+    Meteor.call("certifypk", pk, origin, cb);
+}
 
+
+priv = '{"sym_key":[1614353479,-1322269226,346639468,-1922281664,1543397848,36700503,-1283077055,-1438142375],"sign":"000000530752fdf7419ebc7906d88f682c9a6c027356a95c0c2132","decrypt":"000000de71fe414a477d84ea182a64dc5d24038bab8484f29d0a8e","encrypt":"ee8e81253e2c985137074e514982966787445d47a5013c83b357e3a94a49006ff12db2768bca2f2c9237dc64a977e744","verify":"e16e692df5f28e8f4555c1f25e847d5820d9518a6082ebc76017ff8ad009a818d7395b0c847b9a17b636ce03dce0ac61"}';
+
+idpkeys = deserialize_keys(priv);
+    
 sign_text = function(user, origin, pk) {
     return user + "+++" + origin + "+++" + pk; //TODO: fix this so no formatting attacks possible 
 }
@@ -83,8 +105,8 @@ if (Meteor.isServer) {
       },
       
       // calls cb with a certificate
-      certify_pk : function(pk, origin, cb) {
-	  var c = sign_text(Meteor.user(), pk, origin);
+      certifypk : function(pk, origin, cb) {
+	  var c = sign_text("temp", pk, origin); // USER Meteor.user()
 	  return base_crypto.sign(sign_text, idpkeys.sign);
       },
       
