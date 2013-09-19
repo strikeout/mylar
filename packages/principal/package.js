02dbc4498364b1d9a424d6b541e7a451e970847f
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-09-18 23:47:06 -0400

    stop changing packages/principal/package.js all the time

--- packages/principal/package.js
+++ packages/principal/package.js
@@ -15,33 +15,37 @@ Package.on_use(function (api, where) {
     api.add_files(['crypto_plugin.js', 'idp_helper.js'], 'client');
 
     if(SUPPORT_MULTIKEY){
-      var path = Npm.require('path');
-//START_OF_EXT_FILES
-      api.add_files(path.join('crypto_ext', 'crypto_ext_x86_32.nexe'), 'client');
-      api.add_files(path.join('crypto_ext', 'crypto_ext_x86_64.nexe'), 'client');
-      api.add_files(path.join('crypto_ext', 'crypto_ext_x86_64.map'), 'client');
-      api.add_files(path.join('crypto_ext', 'crypto_ext_x86_32.map'), 'client');
-      api.add_files(path.join('crypto_ext', 'crypto_ext.nmf'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libc.so.b7b14f88'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libc.so.a0e12298'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libppapi_cpp.so'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libpthread.so.a0e12298'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libm.so.a0e12298'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libstdc++.so.6'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libpthread.so.b7b14f88'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libgcc_s.so.1'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/libm.so.b7b14f88'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib32/runnable-ld.so'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libc.so.b7b14f88'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libc.so.a0e12298'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libppapi_cpp.so'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libpthread.so.a0e12298'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libm.so.a0e12298'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libstdc++.so.6'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libpthread.so.b7b14f88'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libgcc_s.so.1'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/libm.so.b7b14f88'), 'client');
-      api.add_files(path.join('crypto_ext', 'lib64/runnable-ld.so'), 'client');
-//END_OF_EXT_FILES
+      // What a hack!  Extract source_root via the exception string.
+      var source_root;
+      var magic_string = '@@nonexistent_file@@';
+      try {
+        Package._require(magic_string);
+      } catch (err) {
+        var msg = err.message;
+        var first_quote = msg.indexOf("'");
+        var path_plus = msg.substr(first_quote + 1);
+        source_root = path_plus.substr(0, path_plus.indexOf(magic_string));
+      }
+
+      var fs = Npm.require('fs');
+      function scan_dir(d) {
+        var files = fs.readdirSync(source_root + d);
+        for (var i = 0; i < files.length; i++) {
+          var f = files[i];
+          var df = d + '/' + files[i];
+          var st = fs.statSync(source_root + df);
+          if (st.isDirectory() && f !== 'main' && f !== 'src') {
+            scan_dir(df);
+          }
+
+          if (st.isFile()) {
+            api.add_files(df, 'client');
+          }
+        }
+      }
+
+      if (fs.existsSync(source_root + 'crypto_ext')) {
+        scan_dir('crypto_ext');
+      }
     }
 });
