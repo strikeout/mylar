06dd210638516d16865427b00925cf836094f987
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-11-27 16:02:35 -0500

    maybe unbreak safari

--- packages/timing/timeLog.js
+++ packages/timing/timeLog.js
@@ -5,6 +5,14 @@ LATENCY_LOG = true; // turn off during tput experiments
 TPUT_LOG = true;
 TOTAL_MSGS = 0;
 
+function now() {
+  if (window.performance && window.performance.now) {
+    return window.performance.now();
+  } else {
+    return 0;
+  }
+}
+
 logAdd = function(msg) {
     if (LATENCY_LOG)
 	timeLog = timeLog + msg;
@@ -12,17 +20,17 @@ logAdd = function(msg) {
 
 startTime = function(msg) {
     if (LATENCY_LOG)
-    	logAdd(msg + " START "  + window.performance.now() + "\n");
+    	logAdd(msg + " START "  + now() + "\n");
 }
 
 endTime = function(msg) {
     if (LATENCY_LOG)
-       logAdd(msg + " END " +  window.performance.now() + "\n");
+       logAdd(msg + " END " +  now() + "\n");
 }
 
 markTime = function(msg, type) {
     if (LATENCY_LOG)
-        logAdd(msg + " " + type + " " + window.performance.now() + "\n");
+        logAdd(msg + " " + type + " " + now() + "\n");
 }
 
 markGlobTime = function(msg, type) {
