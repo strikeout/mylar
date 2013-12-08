aa7ca5618fe8b73d8e4c2c5261bc0e6d91d5ce33
Raluca Ada Popa <raluca@csail.mit.edu>
2013-12-08 18:05:40 -0500

    Rewrote outbound intercept code. We were interposing too early on outgoing data.
    A small issue in Medtest (the required questions showed incomplete for a while after they were completed) pointed to this bigger issue: we were encrypting data before it was being placed in the minicollection and sent to the server. We should instead encrypt just after it is placed in the minicollection because:
    
    1. we don't want encrypted data in the minicollection
    
    2. we want updates in the mini collection to be immediately available/synchronous -- as opposed to waiting until encryption completes (which is asynchronous due to principal lookup)
      --> this was the issue with MedTest: after inserting answers locally, it was fetching these answers from the local collection to see if they were complete; but the asynchronous encryption did not finish by then so it would not see the new result
    
    Meteor's code wasn't clean here: they were doing the local and remote insert in the same function livedata_connection.js:apply and a bit intermingled. I changed this function to separate the two, do the local update, and only then call the intercept code for encryption (before sending data to the server). I plan to clean this code even more -- onto the TODO list.
    
    MedTest issue fixed.

--- packages/mongo-livedata/collection.js
+++ packages/mongo-livedata/collection.js
@@ -379,152 +379,138 @@ var throwIfSelectorIsNotId = function (selector, methodName) {
 // off.
 _.each(["insert", "update", "remove"], function (name) {
   Meteor.Collection.prototype[name] = function (/* arguments */) {
-    var self = this;
-    var args = _.toArray(arguments);
-    var callback;
-    var insertId;
-    var ret;
-
-    if (args.length && args[args.length - 1] instanceof Function)
-      callback = args.pop();
-
-
-      var f = function(meta){
-
-	  // On inserts, always return the id that we generated; on all other
-	  // operations, just return the result from the collection.
-	  var chooseReturnValueFromCollectionResult = function (result) {
-	      if (name === "insert")
-		  return insertId;
-	      else
-		  return result;
-	  };
+      var self = this;
+      var args = _.toArray(arguments);
+      var callback;
+      var insertId;
+      var ret;
+      
+      if (args.length && args[args.length - 1] instanceof Function)
+	  callback = args.pop();
+      
+      var Mylar_meta = {'coll': self, 'transform': intercept_out};
+      
+      if (name === "insert") {
+	  if (!args.length)
+              throw new Error("insert requires an argument");
+	  // shallow-copy the document and generate an ID
+	  args[0] = _.extend({}, args[0]);
+	  if ('_id' in args[0]) {
+              insertId = args[0]._id;
+              if (!insertId || !(typeof insertId === 'string'
+				 || insertId instanceof Meteor.Collection.ObjectID))
+		  throw new Error("Meteor requires document _id fields to be non-empty strings or ObjectIDs");
+	  } else {
+              insertId = args[0]._id = self._makeNewID();
+	  }
+	  Mylar_meta['doc'] = args[0];
+      } else {
+	  args[0] = Meteor.Collection._rewriteSelector(args[0]);
 	  
-	  var wrappedCallback;
-	  if (callback) {
-	      wrappedCallback = function (error, result) {
-		  callback(error, ! error && chooseReturnValueFromCollectionResult(result));
+	  if (name === "update") {
+              // Mutate args but copy the original options object. We need to add
+              // insertedId to options, but don't want to mutate the caller's options
+              // object. We need to mutate `args` because we pass `args` into the
+              // driver below.
+              var options = args[2] = _.clone(args[2]) || {};
+              if (options && typeof options !== "function" && options.upsert) {
+		  // set `insertedId` if absent.  `insertedId` is a Meteor extension.
+		  if (options.insertedId) {
+		      if (!(typeof options.insertedId === 'string'
+			    || options.insertedId instanceof Meteor.Collection.ObjectID))
+			  throw new Error("insertedId must be string or ObjectID");
+		  } else {
+		      options.insertedId = self._makeNewID();
+		  }
+              }
+	      Mylar_meta['doc'] = args[1]['$set']; 
+	  }
+      }
+      
+      
+      // On inserts, always return the id that we generated; on all other
+      // operations, just return the result from the collection.
+      var chooseReturnValueFromCollectionResult = function (result) {
+	  if (name === "insert")
+	      return insertId;
+	  else
+	      return result;
+      };
+      
+      var wrappedCallback;
+      if (callback) {
+	  wrappedCallback = function (error, result) {
+	      callback(error, ! error && chooseReturnValueFromCollectionResult(result));
+	  };
+      }
+
+      if (self._connection && self._connection !== Meteor.server) {
+	  //@ENC: take this branch at client
+	  
+	  // just remote to another endpoint, propagate return value or
+	  // exception.
+	  
+	  //console.log("for " + self._name
+	  //		  + " other server, self._connection._stream keys is "
+	  //		  + self._connection._stream.rawUrl);
+	  
+	  
+	  var enclosing = DDP._CurrentInvocation.get();
+	  var alreadyInSimulation = enclosing && enclosing.isSimulation;
+	  
+	  // console.log("alreadyInSim " + alreadyInSimulation);
+	  if (Meteor.isClient && !wrappedCallback && ! alreadyInSimulation) {
+	      // Client can't block, so it can't report errors by exception,
+	      // only by callback. If they forget the callback, give them a
+	      // default one that logs the error, so they aren't totally
+	      // baffled if their writes don't work because their database is
+	      // down.
+	      // Don't give a default callback in simulation, because inside stubs we
+	      // want to return the results from the local collection immediately and
+	      // not force a callback.
+	      wrappedCallback = function (err) {
+		  if (err)
+		      Meteor._debug(name + " failed: " + (err.reason || err.stack));
 	      };
 	  }
-
-	  console.log("after enc_row, in f");
-	  if (self._connection && self._connection !== Meteor.server) {
-	      //@ENC: take this branch at client
-	      
-	      // just remote to another endpoint, propagate return value or
-	      // exception.
-
-	      //console.log("for " + self._name
-	//		  + " other server, self._connection._stream keys is "
-	//		  + self._connection._stream.rawUrl);
-
-
-	      var enclosing = DDP._CurrentInvocation.get();
-	      var alreadyInSimulation = enclosing && enclosing.isSimulation;
-
-	     // console.log("alreadyInSim " + alreadyInSimulation);
-	      if (Meteor.isClient && !wrappedCallback && ! alreadyInSimulation) {
-		  // Client can't block, so it can't report errors by exception,
-		  // only by callback. If they forget the callback, give them a
-		  // default one that logs the error, so they aren't totally
-		  // baffled if their writes don't work because their database is
-		  // down.
-		  // Don't give a default callback in simulation, because inside stubs we
-		  // want to return the results from the local collection immediately and
-		  // not force a callback.
-		  wrappedCallback = function (err) {
-		      if (err)
-			  Meteor._debug(name + " failed: " + (err.reason || err.stack));
-		  };
-	      }
-	      
-	      if (!alreadyInSimulation && name !== "insert") {
-		  // If we're about to actually send an RPC, we should throw an error if
-		  // this is a non-ID selector, because the mutation methods only allow
-		  // single-ID selectors. (If we don't throw here, we'll see flicker.)
-		  throwIfSelectorIsNotId(args[0], name);
-	      }
-	      
-	      ret = chooseReturnValueFromCollectionResult(
-		  self._connection.apply(self._prefix + name, args, undefined, wrappedCallback, meta)
-	      );
-	      
-	  } else { // @ENC: go on this branch at server
-	      console.log("my server");
-	      // it's my collection.  descend into the collection object
-	      // and propagate any exception.
-	      args.push(wrappedCallback);
-	      try {
-		  // If the user provided a callback and the collection implements this
-		  // operation asynchronously, then queryRet will be undefined, and the
-		  // result will be returned through the callback instead.
-		  console.log("befire queryRet");
-		  var queryRet = self._collection[name].apply(self._collection, args);
-		  console.log("here are the methods of minicollection " +
-			      JSON.stringify(_.keys(self._collection)));
-		  ret = chooseReturnValueFromCollectionResult(queryRet);
-	      } catch (e) {
-		  if (callback) {
-		      callback(e);
-		      ret = null;
-		      return;
-		  }
-		  throw e;
-	      }
+	  
+	  if (!alreadyInSimulation && name !== "insert") {
+	      // If we're about to actually send an RPC, we should throw an error if
+	      // this is a non-ID selector, because the mutation methods only allow
+	      // single-ID selectors. (If we don't throw here, we'll see flicker.)
+	      throwIfSelectorIsNotId(args[0], name);
 	  }
-      };
-
-    var meta = {'coll': self, 'transform': intercept_out};
-      
-    if (name === "insert") {
-      if (!args.length)
-        throw new Error("insert requires an argument");
-      // shallow-copy the document and generate an ID
-      args[0] = _.extend({}, args[0]);
-      if ('_id' in args[0]) {
-        insertId = args[0]._id;
-        if (!insertId || !(typeof insertId === 'string'
-              || insertId instanceof Meteor.Collection.ObjectID))
-          throw new Error("Meteor requires document _id fields to be non-empty strings or ObjectIDs");
-      } else {
-        insertId = args[0]._id = self._makeNewID();
+	  
+	  ret = chooseReturnValueFromCollectionResult(
+	      self._connection.apply(self._prefix + name, args, undefined, wrappedCallback, Mylar_meta)
+	  );
+	  
+      } else { // @ENC: go on this branch at server
+	  // it's my collection.  descend into the collection object
+	  // and propagate any exception.
+	  args.push(wrappedCallback);
+	  try {
+	      // If the user provided a callback and the collection implements this
+	      // operation asynchronously, then queryRet will be undefined, and the
+	      // result will be returned through the callback instead.
+	      var queryRet = self._collection[name].apply(self._collection, args);
+	      ret = chooseReturnValueFromCollectionResult(queryRet);
+	  } catch (e) {
+	      if (callback) {
+		  callback(e);
+		  ret = null;
+		  return;
+	      }
+	      throw e;
+	  }
       }
-	meta['doc'] = args[0];
-    } else {
-      args[0] = Meteor.Collection._rewriteSelector(args[0]);
-
-      if (name === "update") {
-        // Mutate args but copy the original options object. We need to add
-        // insertedId to options, but don't want to mutate the caller's options
-        // object. We need to mutate `args` because we pass `args` into the
-        // driver below.
-        var options = args[2] = _.clone(args[2]) || {};
-        if (options && typeof options !== "function" && options.upsert) {
-          // set `insertedId` if absent.  `insertedId` is a Meteor extension.
-          if (options.insertedId) {
-            if (!(typeof options.insertedId === 'string'
-                  || options.insertedId instanceof Meteor.Collection.ObjectID))
-              throw new Error("insertedId must be string or ObjectID");
-          } else {
-            options.insertedId = self._makeNewID();
-          }
-        }
-	meta['doc'] = args[1]['$set']; // not sure here
-      }
-/*	if (name == "remove") {
-            f();
-	}
-*/
       
-    }
-
-      f(meta);
       
-
-    // both sync and async, unless we threw an exception, return ret
-    // (new document ID for insert, num affected for update/remove, object with
-    // numberAffected and maybe insertedId for upsert).
-    return ret;
+      
+      // both sync and async, unless we threw an exception, return ret
+      // (new document ID for insert, num affected for update/remove, object with
+      // numberAffected and maybe insertedId for upsert).
+      return ret;
   };
 });
 
